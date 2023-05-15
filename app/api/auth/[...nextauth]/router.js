7783891b119c036c3b0ebe4246b6897a78bd5608
amostinanfon <amostinanfon17@gmail.com>
2023-05-15 19:38:31 +0100

    provider configuration

--- app/api/auth/[...nextauth]/router.js
+++ app/api/auth/[...nextauth]/router.js
@@ -1,7 +1,8 @@
+import { connectToDB } from "@/utils/database";
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import { signIn } from "next-auth/react";
-
+import User from "@/models/user";
 
 
 console.log({
@@ -20,11 +21,37 @@ const handler = NextAuth({
         })
     ],
     async session({session}) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
 
     async signIn({profile}){
+        try {
+            await connectToDB();
+            
+            //if user exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
 
+            // if user doesn't exists , create one
+            if (!userExists){
+                await
+                 User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowercase(),
+                    image: profile.picture
+                 })
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 })
 
